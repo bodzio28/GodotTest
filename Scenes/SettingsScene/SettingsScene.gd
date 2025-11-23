04350cb24ceb939db68b32ac11b164452a45f63e
bodzio28 <emodibogdan28@gmail.com>
2025-11-23 15:14:03 +0100

    Dodanie dźwięku i lekkie poprawki w ustawieniach

--- Scenes/SettingsScene/SettingsScene.gd
+++ Scenes/SettingsScene/SettingsScene.gd
@@ -1,10 +1,21 @@
-extends Node
+extends Control
 
-# Ta ścieżka będzie działać na PC i automatycznie w przeglądarce!
+# --- 1. KONFIGURACJA ŚCIEŻEK ---
 const SAVE_PATH = "user://settings.cfg"
-const MAIN_MENU_SCENE = preload("res://Scenes/MainMenu/main.tscn")
+# Upewnij się, że ta ścieżka do menu jest poprawna!
+const MAIN_MENU_SCENE_PATH = "res://Scenes/MainMenu/main.tscn"
 
-# Domyślne wartości
+# --- 2. PRZYPISANIE ELEMENTÓW INTERFEJSU (NODÓW) ---
+# WAŻNE: Sprawdź, czy nazwy po prawej zgadzają się z Twoim drzewem w edytorze!
+@onready var master_slider = $SettingsPanel/SettingsCenter/VSettings/TabContainer/Dźwięk/MasterSlider
+@onready var music_slider = $SettingsPanel/SettingsCenter/VSettings/TabContainer/Dźwięk/MusicSlider
+@onready var sfx_slider = $SettingsPanel/SettingsCenter/VSettings/TabContainer/Dźwięk/SFXSlider
+@onready var mute_box = $SettingsPanel/SettingsCenter/VSettings/TabContainer/Dźwięk/MuteCheckBox
+
+@onready var save_button = $SettingsPanel/SettingsCenter/VSettings/SaveButton
+@onready var return_button = $SettingsPanel/SettingsCenter/VSettings/ReturnButton
+
+# --- 3. DANE (Domyślne ustawienia) ---
 var config_data = {
 	"sound": {
 		"master_volume": 1.0,
@@ -13,27 +24,43 @@ var config_data = {
 		"is_muted": false
 	},
 	"video": {
-		"display_mode": 0, # 0 = W Oknie, 1 = Pełny Ekran, 2 = Bez Ramki
+		"display_mode": 0,
 		"vsync": true
 	}
 }
 
+# --- 4. START SCENY ---
 func _ready():
+	# Najpierw pobieramy ustawienia z pliku
 	load_settings()
-	$SettingsPanel/SettingsCenter/VSettings/ReturnButton.pressed.connect(_on_back_button_pressed)
+	
+	# Aktualizujemy wygląd suwaków, żeby pasowały do wczytanych danych
+	update_ui_elements()
+	
+	# Podłączamy sygnały (reakcje na kliknięcia)
+	# Jeśli podłączyłeś je już w edytorze (zielona ikonka obok funkcji), to te linie można usunąć,
+	# ale bezpieczniej jest zostawić je w kodzie.
+	master_slider.value_changed.connect(_on_master_slider_changed)
+	music_slider.value_changed.connect(_on_music_slider_changed)
+	sfx_slider.value_changed.connect(_on_sfx_slider_changed)
+	
+	if mute_box: # Sprawdzamy czy mute_box istnieje, żeby nie wywaliło błędu
+		mute_box.toggled.connect(_on_mute_toggled)
+	
+	save_button.pressed.connect(save_settings)
+	return_button.pressed.connect(_on_back_button_pressed)
 
-# Wczytuje ustawienia z pliku (lub z Local Storage w przeglądarce)
+# --- 5. LOGIKA DANYCH (Zapis/Odczyt) ---
 func load_settings():
 	var config = ConfigFile.new()
 	var err = config.load(SAVE_PATH)
 
-	# Jeśli nie ma pliku/zapisu, stwórz nowy z domyślnymi
 	if err != OK:
-		print("Nie znaleziono pliku ustawień, tworzę nowy.")
-		save_settings() 
+		print("Brak pliku ustawień. Używam domyślnych.")
+		apply_audio_settings() # Aplikujemy domyślne
 		return
 
-	# Wczytaj zapisane wartości
+	# Pobieramy dane (jeśli czegoś brakuje, wstawiamy wartość domyślną)
 	config_data.sound.master_volume = config.get_value("sound", "master_volume", 1.0)
 	config_data.sound.music_volume = config.get_value("sound", "music_volume", 1.0)
 	config_data.sound.sfx_volume = config.get_value("sound", "sfx_volume", 1.0)
@@ -42,13 +69,14 @@ func load_settings():
 	config_data.video.display_mode = config.get_value("video", "display_mode", 0)
 	config_data.video.vsync = config.get_value("video", "vsync", true)
 
-	# Zastosuj ustawienia natychmiast po wczytaniu
-	apply_all_settings()
+	# Od razu aplikujemy to, co wczytaliśmy (żeby było słychać)
+	apply_audio_settings()
+	apply_video_settings()
 
-# Zapisuje ustawienia do pliku (lub do Local Storage w przeglądarce)
 func save_settings():
 	var config = ConfigFile.new()
 
+	# Wrzucamy aktualne dane do obiektu config
 	config.set_value("sound", "master_volume", config_data.sound.master_volume)
 	config.set_value("sound", "music_volume", config_data.sound.music_volume)
 	config.set_value("sound", "sfx_volume", config_data.sound.sfx_volume)
@@ -57,59 +85,70 @@ func save_settings():
 	config.set_value("video", "display_mode", config_data.video.display_mode)
 	config.set_value("video", "vsync", config_data.video.vsync)
 
+	# Zapisujemy fizycznie na dysk
 	config.save(SAVE_PATH)
+	print("Ustawienia zostały zapisane w: ", SAVE_PATH)
 
-func apply_all_settings():
-	set_volume("Master", config_data.sound.master_volume)
-	set_volume("Music", config_data.sound.music_volume)
-	set_volume("SFX", config_data.sound.sfx_volume)
-	set_mute(config_data.sound.is_muted)
+# --- 6. AKTUALIZACJA UI ---
+func update_ui_elements():
+	# Ustawiamy suwaki w dobrych miejscach
+	master_slider.value = config_data.sound.master_volume
+	music_slider.value = config_data.sound.music_volume
+	sfx_slider.value = config_data.sound.sfx_volume
 	
-	set_display_mode(config_data.video.display_mode)
-	set_vsync(config_data.video.vsync)
+	if mute_box:
+		mute_box.button_pressed = config_data.sound.is_muted
 
+# --- 7. REAKCJA NA SUWAKI (Live Update) ---
+func _on_master_slider_changed(value: float):
+	config_data.sound.master_volume = value
+	_set_bus_volume("Master", value)
 
-# --- Funkcje do zmiany i ZAPISYWANIA ---
+func _on_music_slider_changed(value: float):
+	config_data.sound.music_volume = value
+	_set_bus_volume("Music", value)
 
-func set_volume(bus_name, linear_value):
-	if bus_name == "Master":
-		config_data.sound.master_volume = linear_value
-	elif bus_name == "Music":
-		config_data.sound.music_volume = linear_value
-	elif bus_name == "SFX":
-		config_data.sound.sfx_volume = linear_value
+func _on_sfx_slider_changed(value: float):
+	config_data.sound.sfx_volume = value
+	_set_bus_volume("SFX", value)
 
-	var bus_index = AudioServer.get_bus_index(bus_name)
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(linear_value))
-	save_settings()
-
-func set_mute(is_muted):
+func _on_mute_toggled(is_muted: bool):
 	config_data.sound.is_muted = is_muted
-	var bus_index = AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_mute(bus_index, is_muted)
-	save_settings()
+	var master_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_idx, is_muted)
 
-func set_display_mode(mode_index):
-	config_data.video.display_mode = mode_index
-	match mode_index:
-		0: 
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		1: 
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-		2: 
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	save_settings() 
+# --- 8. FUNKCJE POMOCNICZE (SYSTEMOWE) ---
+func apply_audio_settings():
+	_set_bus_volume("Master", config_data.sound.master_volume)
+	_set_bus_volume("Music", config_data.sound.music_volume)
+	_set_bus_volume("SFX", config_data.sound.sfx_volume)
+	
+	var master_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_idx, config_data.sound.is_muted)
 
-func set_vsync(is_on):
-	config_data.video.vsync = is_on
-	if is_on:
+func _set_bus_volume(bus_name: String, linear_val: float):
+	var bus_idx = AudioServer.get_bus_index(bus_name)
+	# Jeśli linear_val == 0, dajemy -80dB (cisza absolutna), w przeciwnym razie konwertujemy
+	var db_val = linear_to_db(linear_val) if linear_val > 0 else -80.0
+	AudioServer.set_bus_volume_db(bus_idx, db_val)
+
+func apply_video_settings():
+	# V-Sync
+	if config_data.video.vsync:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-	save_settings() 
+		
+	# Tryb okna
+	match config_data.video.display_mode:
+		0: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		1: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		2: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
-
-#Funkcja do powrotu
+# --- 9. NAWIGACJA ---
 func _on_back_button_pressed():
-	print("Powrót do menu głównego...")
-	get_tree().change_scene_to_file("res://Scenes/MainMenu/main.tscn")
+	# Sprawdź czy plik sceny MainMenu faktycznie istnieje pod tą ścieżką!
+	if ResourceLoader.exists(MAIN_MENU_SCENE_PATH):
+		get_tree().change_scene_to_file(MAIN_MENU_SCENE_PATH)
+	else:
+		print("BŁĄD: Nie znaleziono sceny menu pod ścieżką: ", MAIN_MENU_SCENE_PATH)
---
+++ Sounds/SoundScript.gd
@@ -0,0 +1,71 @@
+extends Button
+# --- ŁADOWANIE PLIKÓW ---
+# Upewnij się, że ścieżki są poprawne!
+const AUDIO_HOVER = preload("res://Sounds/Hover.ogg")
+const AUDIO_BUTTON = preload("res://Sounds/Button.ogg")
+const AUDIO_BG_MUSIC = preload("res://Sounds/BackGround.mp3")
+
+# --- ZMIENNE NA ODTWARZACZE ---
+var music_player: AudioStreamPlayer
+var sfx_hover: AudioStreamPlayer
+var sfx_click: AudioStreamPlayer
+
+func _ready() -> void:
+	# 1. Konfiguracja odtwarzaczy przy starcie gry
+	_setup_audio_players()
+	
+	# 2. Start muzyki
+	play_music()
+	
+	# 3. MAGIA: Podłączamy się do sygnału drzewa scen.
+	# Kiedykolwiek jakikolwiek węzeł (Node) zostanie dodany do gry (w dowolnej scenie),
+	# sprawdzimy, czy jest przyciskiem.
+	get_tree().node_added.connect(_on_node_added)
+
+# --- KONFIGURACJA ---
+func _setup_audio_players() -> void:
+	# Muzyka
+	music_player = AudioStreamPlayer.new()
+	music_player.stream = AUDIO_BG_MUSIC
+	music_player.volume_db = -15.0
+	music_player.process_mode = Node.PROCESS_MODE_ALWAYS
+	music_player.bus = "Music"
+	add_child(music_player)
+	
+	# Kliknięcie
+	sfx_click = AudioStreamPlayer.new()
+	sfx_click.stream = AUDIO_BUTTON
+	sfx_click.volume_db = -5.0
+	sfx_click.bus = "SFX"
+	add_child(sfx_click)
+	
+	# Hover
+	sfx_hover = AudioStreamPlayer.new()
+	sfx_hover.stream = AUDIO_HOVER
+	sfx_hover.volume_db = -10.0
+	sfx_hover.bus = "SFX"
+	add_child(sfx_hover)
+
+# --- START MUZYKI ---
+func play_music() -> void:
+	if not music_player.playing:
+		music_player.play()
+
+# --- AUTOMATYCZNE WYKRYWANIE PRZYCISKÓW ---
+func _on_node_added(node: Node) -> void:
+	# Jeśli dodany element to Przycisk (Button) LUB TextureButton...
+	if node is Button or node is TextureButton:
+		# ...i nie podłączyliśmy go jeszcze do naszego systemu dźwiękowego:
+		if not node.mouse_entered.is_connected(_play_hover):
+			# Podłączamy dźwięki
+			node.mouse_entered.connect(_play_hover)
+			node.pressed.connect(_play_click)
+
+# --- ODTWARZANIE EFEKTÓW ---
+func _play_hover() -> void:
+	# Opcjonalny Randomizer, żeby nie brzmiało jak robot
+	sfx_hover.pitch_scale = randf_range(0.95, 1.05)
+	sfx_hover.play()
+
+func _play_click() -> void:
+	sfx_click.play()
